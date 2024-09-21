fa26864209d5edbc044185fb6bfed3f0c7e8c37e
ray <rizkyray01@gmail.com>
2024-09-21 16:38:23 +0800

    new: vercel-1

--- resources/views/frontend/video-section.blade.php
+++ resources/views/frontend/video-section.blade.php
@@ -3,7 +3,7 @@
         <div class="divider divider-neutral text-3xl font-bold text-info">Video</div>
     </div>
 
-    <div>
+    <div class="shadow shadow-lg">
         <iframe src="https://drive.google.com/file/d/1FNt92TXgSIyKtEkYoiM7Pe4-R_QZj8Kn/preview" width="640"
             height="480" allow="autoplay" class="responsive-iframe w-full"></iframe>
 
